735e29675d2fe132b893d0be0b0c34ffeb51b296
yenobi <koshman.animys@gmail.com>
2018-03-15 13:56:41 +0200

    upd component from functional to statefull one, add methods to process form changeing

--- src/Calendar/AddEvent.js
+++ src/Calendar/AddEvent.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function AddEvent(props, {store}) {
-    return (
-        <div>
-            <form>
-                Day:
-                <input placeholder="day" value={store.getState().currentEventDay}/>
-                Text:
-                <textarea placeholder="text" />
+export default class AddEvent extends React.Component {
+    state = {
+        text: ''
+    };
+    
+    handleTextChange = (event) => {
+        this.setState({text: event.target.value})
+    };
+    
+    createEvent = () => {
+        const {store} = this.context;
+        return {
+            day: store.getState().currentEventDay,
+            text: this.state.text
+        };
+    };
+    
+    saveEvent = (ev) => {
+        ev.preventDefault();
+        const {store} = this.context;
+        const event = this.createEvent();
+        store.dispatch({
+            type: 'ADD_EVENT',
+            event
+        });
+        console.log(store.getState());
+    };
+
+    render() {
+        const {store} = this.context;
+        return (
+            <form onSubmit={this.saveEvent}>
+                <div>
+                    <p>Day:</p>
+                    <input placeholder="day" value={store.getState().currentEventDay} readOnly/>
+                    <p>Text:</p>
+                    <textarea placeholder="text" value={this.state.text} onChange={this.handleTextChange}/>
+                    <button type="submit">save event</button>
+                </div>
             </form>
-        </div>
-    )
+        )
+    }
 }
 
 AddEvent.contextTypes = {
